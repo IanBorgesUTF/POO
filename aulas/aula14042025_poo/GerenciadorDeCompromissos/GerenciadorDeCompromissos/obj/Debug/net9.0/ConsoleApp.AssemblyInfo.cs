fed568041cc92bf374c45828d5b658b912009376
IanBorges <ianborges@alunos.utfpr.edu.br>
2025-06-01 21:58:30 -0300

    teste

--- aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abde965a45df330bd8e522de9e63ad4ac63bc166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31577c5f8deb78fb2c7468bf63b2af4bfcc6606")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
